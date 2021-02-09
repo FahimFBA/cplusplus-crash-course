85cc4c94a1fc0cfd64590f2a9246d8ee140fc655
FahimFBA <fahimbinamin@gmail.com>
2021-02-09 19:15:18 +0600

    FIrst CPP

---
+++ HelloWorld.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" << endl; // C++ manipulator endl function is used to insert a new line character and flush the stream. Working of endl manipulator is similar to '\n' character in C++. It prints the output of the following statement in the next line.
+    return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# cplusplus-crash-course
+# C++ Crash Course
+
+<h3 align="center"> Index </h3>
+<hr>
+
+1. [Hello World](HelloWorld.cpp)
+
+
